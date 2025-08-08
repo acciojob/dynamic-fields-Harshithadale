6640433b1c443d264b95d919b77e8422716a05e3
harshithadale <daleharshi6045@gmail.com>
2025-08-08 12:00:48 +0530

    initial-commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,49 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [count, setCount] = useState([1]);
+  const addMore = () => {
+    setCount((prev) => [...prev, 1]);
+  };
+  const deleteItem = (itemToDelete)=>{
+    setCount(count.filter((item,ind)=>ind != itemToDelete))
+ 
+  }
 
-export default App
+ const submitObj = () => {
+    const data = {};
+
+    count.forEach((_, ind) => {
+      const name = document.getElementById(`name${ind}`)?.value || "";
+      const age = document.getElementById(`age${ind}`)?.value || "";
+
+      data[ind] = { name, age };
+    });
+
+    console.log( data);
+  };
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      {count.map((item, ind) => {
+        return (
+          <div key={ind}>
+            <input placeholder="Name" id={`name${ind}`} name = "name"/>
+            <input placeholder="Age" id={`age${ind}`} name="age"/>
+            <button onClick={()=>deleteItem(ind)} >Remove</button>
+          </div>
+        );
+      })}
+      <div>
+        <button onClick={addMore}>Add More</button>
+          <button onClick={submitObj}>Submit</button>
+          
+            <p>After clicking submit check console for data</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
